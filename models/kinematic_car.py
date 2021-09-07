0ca73dca529fa4fca85929868b6fda425f67ef26
Jun Zeng <zengjunsjtu@berkeley.edu>
2021-09-07 13:11:09 -0700

    Add test file for kinematic bicycle model.

---
+++ models/kinematic_car.py
@@ -0,0 +1,113 @@
+import datetime
+
+import matplotlib.patches as patches
+
+from models.geometry_utils import *
+from sim.simulation import *
+
+
+class KinematicCarDynamics:
+    @staticmethod
+    def forward_dynamics(x, u, timestep):
+        """Return updated state in a form of `np.ndnumpy`"""
+        l = 0.1
+        x_next = np.ndarray(shape=(4,), dtype=float)
+        x_next[0] = x[0] + x[2] * math.cos(x[3]) * timestep
+        x_next[1] = x[1] + x[2] * math.sin(x[3]) * timestep
+        x_next[2] = x[2] + u[0] * timestep
+        x_next[3] = x[3] + x[2] * math.tan(u[1]) / l * timestep
+        return x_next
+
+    @staticmethod
+    def forward_dynamics_opt(timestep):
+        """Return updated state in a form of `ca.SX`"""
+        l = 0.1
+        x_symbol = ca.SX.sym("x", 4)
+        u_symbol = ca.SX.sym("u", 2)
+        x_symbol_next = x_symbol[0] + x_symbol[2] * ca.cos(x_symbol[3]) * timestep
+        y_symbol_next = x_symbol[1] + x_symbol[2] * ca.sin(x_symbol[3]) * timestep
+        v_symbol_next = x_symbol[2] + u_symbol[0] * timestep
+        theta_symbol_next = x_symbol[3] + x_symbol[2] * ca.tan(u_symbol[1]) / 0.1 * timestep
+        state_symbol_next = ca.vertcat(x_symbol_next, y_symbol_next, v_symbol_next, theta_symbol_next)
+        return ca.Function("dubin_car_dynamics", [x_symbol, u_symbol], [state_symbol_next])
+
+    @staticmethod
+    def nominal_safe_controller(x, timestep, amin, amax):
+        """Return updated state using nominal safe controller in a form of `np.ndnumpy`"""
+        u_nom = np.zeros(shape=(2,))
+        u_nom[0] = np.clip(-x[2] / timestep, amin, amax)
+        return KinematicCarDynamics.forward_dynamics(x, u_nom, timestep), u_nom
+
+    @staticmethod
+    def safe_dist(x, timestep, amin, amax, dist_margin):
+        """Return a safe distance outside which to ignore obstacles"""
+        # TODO: wrap params
+        safe_ratio = 1.25
+        brake_min_dist = (abs(x[2]) + amax * timestep) ** 2 / (2 * amax) + dist_margin
+        return safe_ratio * brake_min_dist + abs(x[2]) * timestep + 0.5 * amax * timestep ** 2
+
+
+class KinematicCarStates:
+    def __init__(self, x, u=np.array([0.0, 0.0])):
+        self._x = x
+        self._u = u
+
+    def translation(self):
+        return np.array([[self._x[0]], [self._x[1]]])
+
+    def rotation(self):
+        return np.array(
+            [
+                [math.cos(self._x[3]), -math.sin(self._x[3])],
+                [math.sin(self._x[3]), math.cos(self._x[3])],
+            ]
+        )
+
+
+class KinematicCarRectangleGeometry:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self._region = RectangleRegion(-length / 2, length / 2, -width / 2, width / 2)
+
+    def equiv_rep(self):
+        return [self._region]
+
+    def get_plot_patch(self, state):
+        length, width = self._length, self._width
+        x, y, theta = state[0], state[1], state[3]
+        vertices = np.array(
+            [
+                [
+                    x + length / 2 * np.cos(theta) - width / 2 * np.sin(theta),
+                    y + length / 2 * np.sin(theta) + width / 2 * np.cos(theta),
+                ],
+                [
+                    x + length / 2 * np.cos(theta) + width / 2 * np.sin(theta),
+                    y + length / 2 * np.sin(theta) - width / 2 * np.cos(theta),
+                ],
+                [
+                    x - length / 2 * np.cos(theta) + width / 2 * np.sin(theta),
+                    y - length / 2 * np.sin(theta) - width / 2 * np.cos(theta),
+                ],
+                [
+                    x - length / 2 * np.cos(theta) - width / 2 * np.sin(theta),
+                    y - length / 2 * np.sin(theta) + width / 2 * np.cos(theta),
+                ],
+            ]
+        )
+        return patches.Polygon(vertices, alpha=1.0, closed=True, fc="None", ec="tab:brown")
+
+
+class KinematicCarSystem(System):
+    def get_state(self):
+        return self._state._x
+
+    def update(self, unew):
+        xnew = self._dynamics.forward_dynamics(self.get_state(), unew, 0.1)
+        self._state._x = xnew
+        self._time += 0.1
+
+    def logging(self, logger):
+        logger._xs.append(self._state._x)
+        logger._us.append(self._state._u)
---
+++ models/test.py
@@ -0,0 +1,168 @@
+import math
+
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+from control.dcbf_controller import NmpcDcbfController
+from models.dubin_car import (
+    DubinCarDynamics,
+    DubinCarGeometry,
+    DubinCarStates,
+    DubinCarSystem,
+)
+from models.geometry_utils import *
+from models.kinematic_car import (
+    KinematicCarDynamics,
+    KinematicCarRectangleGeometry,
+    KinematicCarStates,
+    KinematicCarSystem,
+)
+from planning.path_generator.search_path_generator import (
+    AstarLoSPathGenerator,
+    AstarPathGenerator,
+    ThetaStarPathGenerator,
+)
+from planning.trajectory_generator.constant_speed_generator import (
+    ConstantSpeedTrajectoryGenerator,
+)
+from sim.simulation import Robot, SingleAgentSimulation
+
+
+def plot_world(simulation):
+    # TODO: make this plotting function general applicable to different systems
+    fig, ax = plt.subplots()
+    plt.axis("equal")
+    global_paths = simulation._robot._global_planner_logger._paths
+    global_path = global_paths[0]
+    ax.plot(global_path[:, 0], global_path[:, 1], "bo--", linewidth=1, markersize=4)
+    closedloop_traj = np.vstack(simulation._robot._system_logger._xs)
+    ax.plot(closedloop_traj[:, 0], closedloop_traj[:, 1], "k-", linewidth=2, markersize=4)
+    for obs in simulation._obstacles:
+        obs_patch = obs.get_plot_patch()
+        ax.add_patch(obs_patch)
+    plt.savefig("figures/world.eps", format="eps", dpi=1000, pad_inches=0)
+    plt.savefig("figures/world.png", format="png", dpi=1000, pad_inches=0)
+
+
+def animate_world(simulation):
+    # TODO: make this plotting function general applicable to different systems
+    fig, ax = plt.subplots()
+    plt.axis("equal")
+    global_paths = simulation._robot._global_planner_logger._paths
+    global_path = global_paths[0]
+    ax.plot(global_path[:, 0], global_path[:, 1], "bo--", linewidth=1, markersize=4)
+
+    local_paths = simulation._robot._local_planner_logger._trajs
+    local_path = local_paths[0]
+    (reference_traj_line,) = ax.plot(local_path[:, 0], local_path[:, 1])
+
+    optimized_trajs = simulation._robot._controller_logger._xtrajs
+    optimized_traj = optimized_trajs[0]
+    (optimized_traj_line,) = ax.plot(optimized_traj[:, 0], optimized_traj[:, 1])
+
+    closedloop_traj = np.vstack(simulation._robot._system_logger._xs)
+    ax.plot(closedloop_traj[:, 0], closedloop_traj[:, 1], "k-", linewidth=2, markersize=4)
+    for obs in simulation._obstacles:
+        obs_patch = obs.get_plot_patch()
+        ax.add_patch(obs_patch)
+
+    robot_patch = patches.Polygon(np.zeros((1, 2)), alpha=1.0, closed=True, fc="None", ec="tab:brown")
+    ax.add_patch(robot_patch)
+
+    def update(index):
+        local_path = local_paths[index]
+        reference_traj_line.set_data(local_path[:, 0], local_path[:, 1])
+        optimized_traj = optimized_trajs[index]
+        optimized_traj_line.set_data(optimized_traj[:, 0], optimized_traj[:, 1])
+        polygon_patch_next = simulation._robot._system._geometry.get_plot_patch(closedloop_traj[index, :])
+        robot_patch.set_xy(polygon_patch_next.get_xy())
+
+    anim = animation.FuncAnimation(fig, update, frames=len(closedloop_traj), interval=1000 * 0.1)
+    anim.save("animation/world.mp4", dpi=300, writer=animation.writers["ffmpeg"](fps=60))
+
+
+def dubin_car_simulation_test():
+    start_pos, goal_pos, grid, obstacles = create_env("maze")
+    robot = Robot(
+        DubinCarSystem(
+            state=DubinCarStates(x=np.block([start_pos[:2], np.array([0.0, start_pos[2]])])),
+            geometry=DubinCarGeometry(0.14, 0.08),
+            dynamics=DubinCarDynamics(),
+        )
+    )
+    # Reduce margin (radius) for tighter corners
+    global_path_margin = 0.07
+    robot.set_global_planner(ThetaStarPathGenerator(grid, quad=False, margin=global_path_margin))
+    robot.set_local_planner(ConstantSpeedTrajectoryGenerator())
+    robot.set_controller(NmpcDcbfController(dynamics=DubinCarDynamics()))
+    sim = SingleAgentSimulation(robot, obstacles, goal_pos)
+    sim.run_navigation(40.0)
+    plot_world(sim)
+    animate_world(sim)
+
+
+def kinematic_car_simulation_test():
+    start_pos, goal_pos, grid, obstacles = create_env("maze")
+    robot = Robot(
+        KinematicCarSystem(
+            state=KinematicCarStates(x=np.block([start_pos[:2], np.array([0.0, start_pos[2]])])),
+            geometry=KinematicCarRectangleGeometry(0.14, 0.08),
+            dynamics=KinematicCarDynamics(),
+        )
+    )
+    # Reduce margin (radius) for tighter corners
+    global_path_margin = 0.07
+    robot.set_global_planner(ThetaStarPathGenerator(grid, quad=False, margin=global_path_margin))
+    robot.set_local_planner(ConstantSpeedTrajectoryGenerator())
+    robot.set_controller(NmpcDcbfController(dynamics=KinematicCarDynamics()))
+    sim = SingleAgentSimulation(robot, obstacles, goal_pos)
+    sim.run_navigation(40.0)
+    plot_world(sim)
+    animate_world(sim)
+
+
+def create_env(env_type):
+    if env_type == "s_path":
+        s = 1.0  # scale of environment
+        start = np.array([0.0 * s, 0.2 * s, 0.0])
+        goal = np.array([1.0 * s, 0.8 * s])
+        bounds = ((-0.2 * s, 0.0 * s), (1.2 * s, 1.2 * s))
+        cell_size = 0.05 * s
+        grid = (bounds, cell_size)
+        obstacles = []
+        obstacles.append(RectangleRegion(0.0 * s, 1.0 * s, 0.9 * s, 1.0 * s))
+        obstacles.append(RectangleRegion(0.0 * s, 0.4 * s, 0.4 * s, 1.0 * s))
+        obstacles.append(RectangleRegion(0.6 * s, 1.0 * s, 0.0 * s, 0.7 * s))
+        return start, goal, grid, obstacles
+    elif env_type == "maze":
+        s = 0.2  # scale of environment
+        start = np.array([0.5 * s, 5.5 * s, -math.pi / 2.0])
+        goal = np.array([12.5 * s, 0.5 * s])
+        bounds = ((0.0 * s, 0.0 * s), (13.0 * s, 6.0 * s))
+        cell_size = 0.25 * s
+        grid = (bounds, cell_size)
+        obstacles = []
+        obstacles.append(RectangleRegion(0.0 * s, 3.0 * s, 0.0 * s, 3.0 * s))
+        obstacles.append(RectangleRegion(1.0 * s, 2.0 * s, 4.0 * s, 6.0 * s))
+        obstacles.append(RectangleRegion(2.0 * s, 6.0 * s, 5.0 * s, 6.0 * s))
+        obstacles.append(RectangleRegion(6.0 * s, 7.0 * s, 4.0 * s, 6.0 * s))
+        obstacles.append(RectangleRegion(4.0 * s, 5.0 * s, 0.0 * s, 4.0 * s))
+        obstacles.append(RectangleRegion(5.0 * s, 7.0 * s, 2.0 * s, 3.0 * s))
+        obstacles.append(RectangleRegion(6.0 * s, 9.0 * s, 1.0 * s, 2.0 * s))
+        obstacles.append(RectangleRegion(8.0 * s, 9.0 * s, 2.0 * s, 4.0 * s))
+        obstacles.append(RectangleRegion(9.0 * s, 12.0 * s, 3.0 * s, 4.0 * s))
+        obstacles.append(RectangleRegion(11.0 * s, 12.0 * s, 4.0 * s, 5.0 * s))
+        obstacles.append(RectangleRegion(8.0 * s, 10.0 * s, 5.0 * s, 6.0 * s))
+        obstacles.append(RectangleRegion(10.0 * s, 11.0 * s, 0.0 * s, 2.0 * s))
+        obstacles.append(RectangleRegion(12.0 * s, 13.0 * s, 1.0 * s, 2.0 * s))
+        obstacles.append(RectangleRegion(0.0 * s, 13.0 * s, 6.0 * s, 7.0 * s))
+        obstacles.append(RectangleRegion(-1.0 * s, 0.0 * s, 0.0 * s, 6.0 * s))
+        obstacles.append(RectangleRegion(0.0 * s, 13.0 * s, -1.0 * s, 0.0 * s))
+        obstacles.append(RectangleRegion(13.0 * s, 14.0 * s, 0.0 * s, 6.0 * s))
+        return start, goal, grid, obstacles
+
+
+if __name__ == "__main__":
+    dubin_car_simulation_test()
+    kinematic_car_simulation_test()
