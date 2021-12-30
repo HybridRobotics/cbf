ebd31566d1803ddbeaa984dedb813db3d07f6638
Jun Zeng <zengjunsjtu@berkeley.edu>
2021-12-30 15:49:22 -0800

    Refactor test file for the model of kinematic car
    1) Rename test file under kinematic_car.py
    2) Move global variables outside the optimizer
    3) Clean up kinematic_car_test.py

--- control/dcbf_controller.py
+++ control/dcbf_controller.py
@@ -3,8 +3,9 @@ from control.dcbf_optimizer import NmpcDbcfOptimizer, NmpcDcbfOptimizerParam
 
 class NmpcDcbfController:
     # TODO: Refactor this class to inheritate from a general optimizer
-    def __init__(self, dynamics=None):
-        self._param = NmpcDcbfOptimizerParam()
+    def __init__(self, dynamics=None, opt_param=None):
+        # TODO: Rename self._param into self._opt_param to avoid ambiguity
+        self._param = opt_param
         self._optimizer = NmpcDbcfOptimizer({}, {}, dynamics.forward_dynamics_opt(0.1))
 
     def generate_control_input(self, system, global_path, local_trajectory, obstacles):
